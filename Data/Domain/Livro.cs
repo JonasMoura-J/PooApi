1716569a1e87022645fe0aa26b958f7cbee47525
JonasMoura-J <jonasmourat9090@gmail.com>
2021-12-09 21:15:42 -0300

    Adicionando projeto

---
+++ Data/Domain/Livro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PooApi.Data
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string ExternalId { get; private set; }
+        public string Nome { get; set; }
+        public  string Autor { get; set; }
+        public int Paginas { get; set; }
+        public Categorias categoria { get; set; }
+
+
+        public enum Categorias
+        {
+            Ficcao = 0,
+            Suspense = 1,
+            Ficcao_Cientifica = 2,
+            Fantasia = 3,
+            Horror = 4,
+            Poesia = 5,
+            Romance = 6
+        }
+    }
+}
